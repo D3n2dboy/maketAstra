c30fc732c5dbb6d19a1976e7698e302299c594c1
d3n2dboy <d3n2dboy@gmail.com>
2025-03-25 23:59:11 +0500

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,11 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+AOS.init({
+    offset: 20,
+    delay: 300,
+    once: true,
+});
+
